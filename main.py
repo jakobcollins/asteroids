87ca94d4229febc32b59922656ad177d756b5a2f
jakobcollins <jakoblcollins@outlook.com>
2025-06-27 10:41:03 +0100

    added draw player functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -7,11 +8,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    x, y = SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 # Center of the screen
+    player = Player(x, y)
+
     clock = pygame.time.Clock()  # Stores clock object in a variable
     dt = 0 # Delta time
 
     while True:
         Fill(screen, (0, 0, 0))  # Fill the screen with black
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
