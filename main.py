6fb56b43da97e4e714a7279633b66f4e54004f2b
jakobcollins <jakoblcollins@outlook.com>
2025-06-27 10:13:07 +0100

    added clock object and delta time variable

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()  # Stores clock object in a variable
+    dt = 0 # Delta time
+
     while True:
         Fill(screen, (0, 0, 0))  # Fill the screen with black
         pygame.display.flip()
@@ -13,8 +17,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-def Fill(screen, color):
-    # Fills the screen with the specified color
+        dt = clock.tick(60) / 1000 # Gets the time since the last frame in seconds
+
+def Fill(screen, color): # Fills the screen with the specified colour
     screen.fill(color)
 
 if __name__ == "__main__":
